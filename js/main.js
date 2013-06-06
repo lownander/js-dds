adf9fa3e915e1278004c986daffc5a0bcee72896
Emil <admin@Emils-Macbook-PRO-2.local>
2013-06-06 20:53:01 +0200

    progress
    worked on the tasks

--- js/main.js
+++ js/main.js
@@ -3,19 +3,29 @@ $(function() {
   //////////////
   // EXERCISE 2
   // Add your code below here so that an alert message pops up when you press that button
-
-
+  
   //////////////
   // EXERCISE 3
 
   /// "Fade out" spell
+  $("#disappear").click(function(){
+    $("img.dolls").fadeOut();
+  });
 
   /// "Fade in" spell
-
+  $("#appear").click(function(){
+    $("img.dolls").fadeIn();
+  });
   // "Transformation" spell
-
+  $("#transform").click(function(){
+    $("#voldemort").fadeOut(function(){
+      $("#cats").fadeIn();
+    }); 
+  });
   // "Hover" spell
-
+  $("#levitate").hover(function(){
+    $("#carola").toggleClass("levitating");
+  });
   //////////////
   // EXERCISE 4
 
@@ -25,18 +35,24 @@ $(function() {
   function addToCart() {
     console.log("added an item!");
     numItems = numItems + 1;
+    $("#cart").fadeOut(function(){
+      $("#cart-quantity").html(numItems);
+    }).fadeIn();
     // add code to make the cart fade out and fade in
     // add code to insert the value of numItems in the html
   }
+  $("#add-to-cart").click(addToCart);
 
   /////////////
   // EXERCISE 6
   $('#spotify-search').click(function() {
     // You need to add code to get the text from the input field.
-    var query = "http://ws.spotify.com/search/1/track?q=sussudio";
+    var song = $("#song").val();
+    var query = "http://ws.spotify.com/search/1/track?q=" + song;
     $.getJSON(query, function(data) {
       $.each(data.tracks, function(index, track) {
         // You need to add code to add each track name to the list of results.
+        $("#spotify-results").append(track.name);
       });
     });
   });
